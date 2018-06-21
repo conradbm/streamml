db893e97f2ac7886e5d759c0a323ebd64cd2292c
Blake Conrad <bmconrad@iupui.edu>
2018-06-21 13:52:21 -0500

    updates to feature support

--- tester.py
+++ tester.py
@@ -1,24 +1,27 @@
 
 import pandas as pd
 import numpy as np
-import sys
-sys.path.insert(2, 'C:\\Users\\1517766115.CIV\\Desktop\\streamml')
+# FOR ANY SYSTEM: INCLUDE STREAMML
+#import sys
+#sys.path.insert(2, 'C:\\Users\\1517766115.CIV\\Desktop\\streamml')
 from streamline.transformation.flow.TransformationStream import TransformationStream
 from streamline.model_selection.flow.ModelSelectionStream import ModelSelectionStream
 
 # git checkout -b modelSelectionUpdates
 # git push -u origin modelSelectionUpdates
 
+# FOR MAC:
 # nano ~/.bash_profile
 # export PYTHONPATH="${PYTHONPATH}:/Users/bmc/Desktop/streamml"
 # source ~/.bash_profile
-# python -W ignore tester.py 
+# python -W ignore tester.py
+
 #X = pd.DataFrame(np.matrix([[np.random.exponential() for j in range(10)] for i in range(200)]))
 #y = pd.DataFrame(np.array([np.random.exponential() for i in range(200)]))
 
-#D = pd.read_csv("Series3_6.15.17_padel.csv")
-#X = D.iloc[:,2:]
-#y = D.iloc[:,1]
+D = pd.read_csv("Series3_6.15.17_padel.csv")
+X = D.iloc[:,2:]
+y = D.iloc[:,1]
 
 ynakiller = y.isna()
 X = X.loc[-ynakiller,:]
@@ -26,18 +29,51 @@ y = y.loc[-ynakiller]
 X.replace([np.nan, np.inf, -np.inf],0, inplace=True)
 
 print(X.shape)
-#X = pd.DataFrame(np.matrix([[np.random.exponential() for j in range(10)] for i in range(200)]))
-#y = pd.DataFrame(np.array([np.random.exponential() for i in range(200)]))
+print (y.shape)
+
+"""
+Supported Transformations:
+["scale","normalize","boxcox","binarize","pca","kmeans"]
+http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
+http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.Normalizer.html
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.boxcox.html
+http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.Binarizer.html
+http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
+http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+
+To be implemented:
+["brbm"] --> Latent representations of the data
+http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.BernoulliRBM.html#sklearn.neural_network.BernoulliRBM
+"""
 Xnew = TransformationStream(X).flow(["scale","normalize","pca", "kmeans"], 
                                     params={"pca__percent_variance":0.75, 
                                             "kmeans__n_clusters":2},
                                    verbose=True)
 
 #preproc options: scale, normalize, boxcox, binarize, pca, kmeans
-#model options: lr, ridge, lasso, enet, svr, knnr, abr, rfr
+#model options: 
 #error options: 'mean_squared_error','r2'
 
 #scoring option not working right, be okay with default scorers.
+
+"""
+Supported Models:
+["lr", "ridge", "lasso", "enet", "svr", "knnr", "abr", "rfr"]
+http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
+http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Ridge.html
+http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Lasso.html
+http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.ElasticNet.html
+http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVR.html
+http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsRegressor.html
+http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostRegressor.html
+http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
+
+To be implemented
+["mlpr", "dtr"]
+http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPRegressor.html#sklearn.neural_network.MLPRegressor
+http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeRegressor.html#sklearn.tree.DecisionTreeRegressor
+"""
+
 performances = ModelSelectionStream(Xnew,y).flow(["svr", "lr", "knnr","lasso","abr"],
                                               params={'svr__C':[1,0.1,0.01,0.001],
                                                       'svr__gamma':[0, 0.01, 0.001, 0.0001],
@@ -50,4 +86,5 @@ performances = ModelSelectionStream(Xnew,y).flow(["svr", "lr", "knnr","lasso","a
                                                 metrics=['r2','rmse'],
                                                 verbose=True,
                                                 regressors=True)
+                                                
 print(performances)
