a55308fa44f58f831e9e7fa768e1312fb53db98c
Blake Conrad <bmc.cs@outlook.com>
2019-01-19 11:24:09 -0600

    testing scikit-criteria vs. skcriteria

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
           'sklearn',
           'matplotlib',
           'seaborn',
-          'skcriteria'
+          'scikit-criteria'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
